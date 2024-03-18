66e6b7bfd19ce63f047ea3b4c5634258990db755
Elizavetha <AlisaDragonShevchenko@gmail.com>
2024-03-19 01:06:01 +0200

    master

--- JS-5.js
+++ JS-5.js
@@ -1,97 +1,61 @@
-let array = [16,-37,54,-4,72,-56,47,4,-16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
+let arr = [16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54, 76, -4, 12, -35, 4, 47];
+let sumPositive = 0;
+let countPositive = 0;
+let minElement = arr[0];
+let minElementIndex = 0;
+let maxElement = arr[0];
+let maxElementIndex = 0;
+let countNegative = 0;
+let countOddPositive = 0;
+let countEvenPositive = 0;
+let sumEvenPositive = 0;
+let sumOddPositive = 0;
+let productPositive = 1;
+let maxElementValue = -Infinity;
 
-
-let sum = 0;
-let positiveCount = 0;
-for (let i = 0; i < array.length; i++) {
-    if (array[i] > 0) {
-        sum += array[i];
-        positiveCount++;
+arr.forEach((element, index) => {
+    if (element > 0) {
+        sumPositive += element;
+        countPositive++;
+        productPositive *= element;
+        if (element % 2 === 0) {
+            countEvenPositive++;
+            sumEvenPositive += element;
+        } else {
+            countOddPositive++;
+            sumOddPositive += element;
+        }
+    } else {
+        countNegative++;
     }
-}
-alert(`Сума позитивних елементів: ${sum}, кількість позитивних елементів: ${positiveCount}`);
 
-
-let min = array[0];
-let minIndex = 0;
-for (let i = 1; i < array.length; i++) {
-    if (array[i] < min) {
-        min = array[i];
-        minIndex = i;
+    if (element < minElement) {
+        minElement = element;
+        minElementIndex = index;
     }
-}
-alert(`Мінімальний елемент масиву: ${min}, його порядковий номер: ${minIndex}`);
 
-
-let max = array[0];
-let maxIndex = 0;
-for (let i = 1; i < array.length; i++) {
-    if (array[i] > max) {
-        max = array[i];
-        maxIndex = i;
+    if (element > maxElement) {
+        maxElement = element;
+        maxElementIndex = index;
     }
-}
-alert(`Максимальний елемент масиву: ${max}, його порядковий номер: ${maxIndex}`);
 
-
-let negativeCount = 0;
-for (let i = 0; i < array.length; i++) {
-    if (array[i] < 0) {
-        negativeCount++;
+    if (element > maxElementValue) {
+        maxElementValue = element;
     }
-}
-alert(`Кількість негативних елементів: ${negativeCount}`);
 
-
-let oddPositiveCount = 0;
-for (let i = 0; i < array.length; i++) {
-    if (array[i] > 0 && array[i] % 2 !== 0) {
-        oddPositiveCount++;
+    if (element < maxElementValue) {
+        arr[index] = 0;
     }
-}
-alert(`Кількість непарних позитивних елементів: ${oddPositiveCount}`);
+});
 
-
-let evenPositiveCount = 0;
-for (let i = 0; i < array.length; i++) {
-    if (array[i] > 0 && array[i] % 2 === 0) {
-        evenPositiveCount++;
-    }
-}
-alert(`Кількість парних позитивних елементів: ${evenPositiveCount}`);
-
-
-let evenPositiveSum = 0;
-for (let i = 0; i < array.length; i++) {
-    if (array[i] > 0 && array[i] % 2 === 0) {
-        evenPositiveSum += array[i];
-    }
-}
-alert(`Сума парних позитивних елементів: ${evenPositiveSum}`);
-
-
-let oddPositiveSum = 0;
-for (let i = 0; i < array.length; i++) {
-    if (array[i] > 0 && array[i] % 2 !== 0) {
-        oddPositiveSum += array[i];
-    }
-}
-alert(`Сума непарних позитивних елементів: ${oddPositiveSum}`);
-
-
-let product = 1;
-for (let i = 0; i < array.length; i++) {
-    if (array[i] > 0) {
-        product *= array[i];
-    }
-}
-alert(`Добуток позитивних елементів: ${product}`);
-
-
-let maxElement = Math.max(...array);
-for (let i = 0; i < array.length; i++) {
-    if (array[i] !== maxElement) {
-        array[i] = 0;
-    }
-}
-alert(`Найбільший серед елементів масиву: ${maxElement}, масив після обнулення: [${array}]`);
+alert(`Сума позитивних елементів: ${sumPositive}`);
+alert(`Кількість позитивних елементів: ${countPositive}`);
+alert(`Мінімальний елемент: ${minElement}, порядковий номер: ${minElementIndex}`);
+alert(`Максимальний елемент: ${maxElement}, порядковий номер: ${maxElementIndex}`);
+alert(`Кількість негативних елементів: ${countNegative}`);
+alert(`Кількість непарних позитивних елементів: ${countOddPositive}`);
+alert(`Кількість парних позитивних елементів: ${countEvenPositive}`);
+alert(`Сума парних позитивних елементів: ${sumEvenPositive}`);
+alert(`Сума непарних позитивних елементів: ${sumOddPositive}`);
+alert(`Добуток позитивних елементів: ${productPositive}`);
+alert(`Найбільший серед елементів масиву: ${maxElementValue}, решта елементів обнулені: ${arr}`);
