178200f295e93d72e8809bcf9baa604452c26777
Elizavetha <AlisaDragonShevchenko@gmail.com>
2024-05-18 01:32:04 +0300

    master

--- JS-21.js
+++ JS-21.js
@@ -1,23 +1,21 @@
-function* Foo() {
-    console.log('start');
-    yield 1;
-    yield 2;
-    yield 3;
-    console.log('finish');
-  }
-  
-  let iterator = Foo();
-  console.log(iterator.next().value); // 1
-  console.log(iterator.next().done);  // false
-  
-  function* Boo() {
-    console.log('start');
-    yield 1;
-    yield 2;
-    yield 3;
-    console.log('finish');
-  }
-  
-  iterator = Boo();
-  console.log(iterator.next().value); // 1
-  console.log(iterator.next().done);  // false  
+function Boo() {
+  let index = 0;
+  const values = [1, 2, 3];
+
+  return {
+    next: function() {
+      if (index < values.length) {
+        return { done: false, value: values[index++] };
+      } else {
+        return { done: true };
+      }
+    }
+  };
+}
+
+const iterator = Boo();
+console.log(iterator.next().value); // 1
+console.log(iterator.next().done); // false
+console.log(iterator.next().value); // 2
+console.log(iterator.next().value); // 3
+console.log(iterator.next().done); // true
